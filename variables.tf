2edb7aae72a45f95771f5a80aba79e7d0387da37
Julie Allinson <julie.allinson@london.ac.uk>
2019-04-11 15:27:38 +0000

    pvc updates

--- variables.tf
+++ variables.tf
@@ -5,7 +5,9 @@ variable "client_certificate" { }
 variable "client_key" { }
 variable "cluster_ca_certificate" { }
 variable "volume" {}
-variable "mount_size" {}
+variable "mount_size" {
+  default = "1G"
+}
 variable "storage_class_name" { 
   default = "azurefile"
 }
