b5d32fb9c2351c9b3d68663f568675c4d710ff8d
Rory McNicholl <rory.mcnicholl@london.ac.uk>
2023-04-03 18:52:20 +0000

    main to 0.12

--- output.tf
+++ output.tf
@@ -1,6 +1,6 @@
 output "pvc_claim_name" {
-  value = "${kubernetes_persistent_volume_claim.default.metadata.0.name}"
+  value = kubernetes_persistent_volume_claim.default.metadata.0.name
 }
 output "volume_name" {
-  value = "${kubernetes_persistent_volume_claim.default.spec.0.volume_name}"
+  value = kubernetes_persistent_volume_claim.default.spec.0.volume_name
 }
