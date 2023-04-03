10cede838f2978b0059edae276a0d84f23f4d127
Rory McNicholl <rory.mcnicholl@london.ac.uk>
2023-04-03 13:30:35 +0100

    branch this next time :|

--- main.tf
+++ main.tf
@@ -1,24 +1,24 @@
 provider "kubernetes" {
  # load_config_file = false
-  host = var.host
-  username = var.username
-  password = var.password
-  client_certificate = var.client_certificate
-  client_key = var.client_key
-  cluster_ca_certificate = var.cluster_ca_certificate
+  host = "${var.host}"
+  username = "${var.username}"
+  password = "${var.password}"
+  client_certificate = "${var.client_certificate}"
+  client_key = "${var.client_key}"
+  cluster_ca_certificate = "${var.cluster_ca_certificate}"
 }
 
 resource "kubernetes_persistent_volume_claim" "default" {
   metadata {
-    name = var.volume
+    name = "${var.volume}"
   }
   spec {
     access_modes = ["ReadWriteOnce"]
     resources {
       requests = {
-        storage = var.mount_size
+        storage = "${var.mount_size}"
       }
     }
-    storage_class_name = var.storage_class_name
+    storage_class_name = "${var.storage_class_name}"
   }
 }
